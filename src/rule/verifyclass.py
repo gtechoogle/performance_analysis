ce81be6a4700a710cbf20918598936789cb44779
peng.li <peng.li@gmail.com>
2018-03-13 23:36:36 +0800

    add more function
    add more function

---
+++ src/rule/verifyclass.py
@@ -0,0 +1,8 @@
+class VerifyClass(object):
+    source_data = []
+    target_data = []
+    def __init__(self, source, target):
+        self.source_data = source;
+        self.target_data = target;
+    def checkverifyclass(pkg):
+        
--- src/trace.py
+++ src/trace.py
@@ -5,33 +5,58 @@ class Trace(object):
     cpu_num = 0
     time_stamp = 0
     function_name = ''
-    def __init__(self, rawdata):
+    argument = ''
+    def __init__(self, rowdata):
+        rawdata = self.parserowdata(rowdata)
+        # print rawdata
         self.task = self.gettask(rawdata)
         self.pid = self.getpid(rawdata)
         self.tgid = self.gettgid(rawdata)
         self.cpu_num = self.getcpunum(rawdata)
         self.time_stamp = self.gettime(rawdata)
         self.function_name = self.getfunctionname(rawdata)
+        self.argument = self.getargument(rawdata)
+    def parserowdata(self, data):
+        temp = data.split(' ')
+        value = []
+        for x in temp:
+            if(x != ''):
+                value.append(x)
+        if value[1] == '(':
+            value.pop(1);
+        return value
+        
     def print_data(self):
         print ("task = " + self.task + " pid = " + self.pid +
-        " tgid = " + self.tgid + " cpunum = %d"%self.cpu_num + " time = " + self.time_stamp)
+        " tgid = " + self.tgid + " cpunum = %d"%self.cpu_num + " time = %f"%self.time_stamp + " function_name = " + self.function_name + " argument = " + self.argument)
     
-    def gettask(self,str):
+    def gettask(self,data):
+        str = date[0]
         task = str[0:str.find('-')]
         return task.strip(' ')
-    def getpid(self,str):
-        pid = str[str.find('-') + 1 : str.find(' ')]
+    def getpid(self,data):
+        str = data[0]
+        pid = str[str.find('-') + 1 :]
         return pid.strip(' ')
-    def gettgid(self,str):
-        tgid = str[str.find('(') + 1:str.find(')')]
-        return tgid.strip(' ')
+    def gettgid(self,data):
+        tgid = ''
+        if data[1].find('(')!= -1 or data[1].find(')')!= -1:
+            tgid = data[1].strip('(').strip(')').strip(' ')
+        return tgid
     def getcpunum(self,str):
+        # print str
         cpunum = str[str.find('[') + 1:str.find(']')]
+        # print str
+        # print cpunum
         return int(cpunum.strip(' '))
     def gettime(self,str):
-        index1 = str.find(':')
-        index2 = str.rfind(' ',index1)
-        return str[index2:index1]
+        print str
+        return float(str.strip(':'))
     def getfunctionname(self,str):
-        return ''
+        return str.strip(':')
+    def getargument(self,str):
+        temp = ''
+        for x in str[6:len(str)]:
+            temp = temp + x + ' '
+        return temp
         
